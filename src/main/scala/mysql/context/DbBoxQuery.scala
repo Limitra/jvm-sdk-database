00927ce20d529aa837f4a72603ede8daf418a916
Mustafa Nevfel <mnevfel@limitra.com>
2019-06-23 18:05:48 +0300

    Base class of generic table query pinned.

---
+++ src/main/scala/mysql/context/DbBoxQuery.scala
@@ -0,0 +1,13 @@
+package com.limitra.sdk.database.mysql
+
+import slick.jdbc.MySQLProfile.api._
+
+import scala.language.experimental.macros
+
+/**
+  * That enables us to use the DbBoxQuery class as an TableQuery.
+  * In this way we can add new features without disabling any features.
+ */
+protected abstract class DbBoxQuery[E <: BaseBox, T <: BaseBoxTable[E]](query: TableQuery[T], cons: Tag => T) extends TableQuery[T](cons) {
+  val TableName = this.baseTableRow.tableName
+}
