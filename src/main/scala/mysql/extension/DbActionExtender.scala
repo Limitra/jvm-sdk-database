35978b94054152a988bd3adba61f129b8fa24df4
Mustafa Nevfel <mnevfel@limitra.com>
2019-06-23 18:10:28 +0300

    DBIOAction operation extensions generated.

---
+++ src/main/scala/mysql/extension/DbActionExtender.scala
@@ -0,0 +1,36 @@
+package com.limitra.sdk.database.mysql
+
+import slick.jdbc.MySQLProfile.api._
+import slick.jdbc.MySQLProfile.backend.DatabaseDef
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+import scala.language.experimental.macros
+
+abstract class DbActionExtender[R](db: DatabaseDef, action: DBIOAction[R, NoStream, Nothing]) {
+  // Action executer extension method for sync request.
+  // This extension returns action value directly.
+  def Save(): R = {
+    try {
+      Await.result(db.run(action), Duration.Inf)
+    } catch {
+      case e: Exception => {
+        println(e.getMessage)
+        throw new RuntimeException(e.getMessage)
+      }
+    }
+  }
+
+  // Query executer extension method for async request.
+  // This extension doesnt returns query value with callback.
+  def SaveAsync(): Future[R] = {
+    try {
+      return db.run(action)
+    } catch {
+      case e: Exception => {
+        println(e.getMessage)
+        throw new RuntimeException(e.getMessage)
+      }
+    }
+  }
+}
