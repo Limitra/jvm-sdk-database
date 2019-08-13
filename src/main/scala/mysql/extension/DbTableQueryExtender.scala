d4ebd1e1820a02fc2aebf96ebffc72cd6afcf54a
Mustafa Nevfel <me@mnevfel.com>
2019-08-13 15:46:55 +0300

    Delete operations zero effected fk errors non-throw overload applied.

--- src/main/scala/mysql/extension/DbTableQueryExtender.scala
+++ src/main/scala/mysql/extension/DbTableQueryExtender.scala
@@ -80,12 +80,12 @@ protected abstract class DbTableQueryExtender[E <: BaseBox, T <: BaseBoxTable[E]
 
   // Runs customized delete by id action sync
   def Delete(id: Long): Int = {
-    this._delete(id).Save
+    this._delete(id).Save(0)
   }
 
   // Runs customized delete by id action sync
   def DeleteAsync(id: Long) = {
-    this._delete(id).SaveAsync
+    this._delete(id).SaveAsync(0)
   }
 
   // Generates customized query action for delete by expression operation
@@ -95,12 +95,12 @@ protected abstract class DbTableQueryExtender[E <: BaseBox, T <: BaseBoxTable[E]
 
   // Runs customized delete by expression action sync
   def Delete[Q <: Rep[_]](expr: T => Q)(implicit wt: CanBeQueryCondition[Q]) = {
-    this._delete(expr).Save
+    this._delete(expr).Save(0)
   }
 
   // Runs customized delete by expression action sync
   def DeleteAsync[Q <: Rep[_]](expr: T => Q)(implicit wt: CanBeQueryCondition[Q]) = {
-    this._delete(expr).SaveAsync
+    this._delete(expr).SaveAsync(0)
   }
 
   // Generates customized query action for delete all operation
@@ -110,11 +110,11 @@ protected abstract class DbTableQueryExtender[E <: BaseBox, T <: BaseBoxTable[E]
 
   // Runs customized delete all action sync
   def DeleteAll: Int = {
-    this._deleteAll.Save
+    this._deleteAll.Save(0)
   }
 
   // Runs customized delete all action sync
   def DeleteAllAsync(callback: (Int) => Unit = null) = {
-    this._deleteAll.SaveAsync
+    this._deleteAll.SaveAsync(0)
   }
 }
