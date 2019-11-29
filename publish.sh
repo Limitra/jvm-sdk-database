b17e49594595b77280309d07e36fdfdd78236247
Mustafa Nevfel <me@mnevfel.com>
2019-11-29 03:11:08 +0300

    Release configuration files added to source.

--- publish.sh
+++ publish.sh
@@ -5,7 +5,9 @@ cp proj-build.sbt build.sbt #Copy build.sbt for include pom config.
 cat pom.temp >> build.sbt #Combine configuration files project and pom.
 
 sbt clean update compile #Compile with new configuration.
+
 sbt publishSigned #Publish staging.
+sbt sonatypeRelease #Publish public central.
 
 rm build.sbt #Remove configuration temp file.
 mv proj-build.sbt build.sbt #Restore original build.sbt.
