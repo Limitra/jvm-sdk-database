dd12302b3ced33023a1fe40fe61fb9f2d688537f
Mustafa Nevfel <me@mnevfel.com>
2020-09-08 04:52:16 +0300

    Conflict methods removed and method names edited.

--- src/main/scala/mysql/extension/DbTableQueryExtender.scala
+++ src/main/scala/mysql/extension/DbTableQueryExtender.scala
@@ -28,13 +28,13 @@ protected abstract class DbTableQueryExtender[E <: BaseBox, T <: BaseBoxTable[E]
   }
 
   // Runs customized insert action sync
-  def Insert(entity: E, incStart: Long = 1): Long = {
-    this._insert(entity, incStart).Save
+  def insert(entity: E, incStart: Long = 1): Long = {
+    this._insert(entity, incStart).save
   }
 
   // Runs customized insert action async
-  def InsertAsync(entity: E, incStart: Long = 1) = {
-    this._insert(entity, incStart).SaveAsync
+  def insertAsync(entity: E, incStart: Long = 1) = {
+    this._insert(entity, incStart).saveAsync
   }
 
   // Generates customized query action for bulk insert operation
@@ -48,13 +48,13 @@ protected abstract class DbTableQueryExtender[E <: BaseBox, T <: BaseBoxTable[E]
   }
 
   // Runs customized bulk insert action sync
-  def BulkInsert(entities: Seq[E], incStart: Long = 1): Seq[Long] = {
-    this._bulkInsert(entities, incStart).Save
+  def bulkInsert(entities: Seq[E], incStart: Long = 1): Seq[Long] = {
+    this._bulkInsert(entities, incStart).save
   }
 
   // Runs customized bulk insert action async
-  def BulkInsertAsync(entities: Seq[E], incStart: Long = 1) = {
-    this._bulkInsert(entities, incStart).SaveAsync
+  def bulkInsertAsync(entities: Seq[E], incStart: Long = 1) = {
+    this._bulkInsert(entities, incStart).saveAsync
   }
 
   // Generates customized query action for update operation
@@ -63,13 +63,13 @@ protected abstract class DbTableQueryExtender[E <: BaseBox, T <: BaseBoxTable[E]
   }
 
   // Runs customized update action sync
-  def Update(entity: E): Int = {
-    this._update(entity).Save
+  def update(entity: E): Int = {
+    this._update(entity).save
   }
 
   // Runs customized update action async
-  def UpdateAsync(entity: E) = {
-    this._update(entity).SaveAsync
+  def updateAsync(entity: E) = {
+    this._update(entity).saveAsync
   }
 
   // Generates customized query action for delete by id operation
@@ -78,13 +78,13 @@ protected abstract class DbTableQueryExtender[E <: BaseBox, T <: BaseBoxTable[E]
   }
 
   // Runs customized delete by id action sync
-  def Delete(id: Long): Int = {
-    this._delete(id).Save(0)
+  def delete(id: Long): Int = {
+    this._delete(id).save(0)
   }
 
   // Runs customized delete by id action sync
-  def DeleteAsync(id: Long) = {
-    this._delete(id).SaveAsync(0)
+  def deleteAsync(id: Long) = {
+    this._delete(id).saveAsync(0)
   }
 
   // Generates customized query action for delete by expression operation
@@ -93,13 +93,13 @@ protected abstract class DbTableQueryExtender[E <: BaseBox, T <: BaseBoxTable[E]
   }
 
   // Runs customized delete by expression action sync
-  def Delete[Q <: Rep[_]](expr: T => Q)(implicit wt: CanBeQueryCondition[Q]) = {
-    this._delete(expr).Save(0)
+  def delete[Q <: Rep[_]](expr: T => Q)(implicit wt: CanBeQueryCondition[Q]) = {
+    this._delete(expr).save(0)
   }
 
   // Runs customized delete by expression action sync
-  def DeleteAsync[Q <: Rep[_]](expr: T => Q)(implicit wt: CanBeQueryCondition[Q]) = {
-    this._delete(expr).SaveAsync(0)
+  def deleteAsync[Q <: Rep[_]](expr: T => Q)(implicit wt: CanBeQueryCondition[Q]) = {
+    this._delete(expr).saveAsync(0)
   }
 
   // Generates customized query action for delete all operation
@@ -108,12 +108,12 @@ protected abstract class DbTableQueryExtender[E <: BaseBox, T <: BaseBoxTable[E]
   }
 
   // Runs customized delete all action sync
-  def DeleteAll: Int = {
-    this._deleteAll.Save(0)
+  def deleteAll: Int = {
+    this._deleteAll.save(0)
   }
 
   // Runs customized delete all action sync
-  def DeleteAllAsync(callback: (Int) => Unit = null) = {
-    this._deleteAll.SaveAsync(0)
+  def deleteAllAsync(callback: (Int) => Unit = null) = {
+    this._deleteAll.saveAsync(0)
   }
 }
