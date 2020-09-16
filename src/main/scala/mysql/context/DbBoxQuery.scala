2228dcb5df9661b9d1636dc997ab519147ee4338
Mustafa Nevfel <me@mnevfel.com>
2020-09-16 23:31:19 +0300

    Rename reverted.

--- src/main/scala/mysql/context/DbBoxQuery.scala
+++ src/main/scala/mysql/context/DbBoxQuery.scala
@@ -8,7 +8,7 @@ import scala.language.experimental.macros
   * That enables us to use the DbBoxQuery class as an TableQuery.
   * In this way we can add new features without disabling any features.
  */
-protected abstract class DbBoxQuery[E <: BaseBox, T <: BaseBoxTable[E]](q: TableQuery[T], cons: Tag => T) extends TableQuery[T](cons) {
-  val tableName = this.baseTableRow.tableName
-  val query = q.to[Seq]
+protected abstract class DbBoxQuery[E <: BaseBox, T <: BaseBoxTable[E]](query: TableQuery[T], cons: Tag => T) extends TableQuery[T](cons) {
+  val TableName = this.baseTableRow.tableName
+  val Query = query.to[Seq]
 }
