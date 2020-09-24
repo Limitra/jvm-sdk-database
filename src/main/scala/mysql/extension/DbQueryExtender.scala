06da4bab925fd4ba388054c34038f0098e21ac98
Mustafa Nevfel <me@mnevfel.com>
2020-09-24 16:11:47 +0300

    Removed ref extensions.

--- src/main/scala/mysql/extension/DbQueryExtender.scala
+++ src/main/scala/mysql/extension/DbQueryExtender.scala
@@ -27,16 +27,6 @@ protected abstract class DbQueryExtender[+T, E](db: DatabaseDef, query: Query[T,
     this.query.result.headOption.save
   }
 
-  // Generates mapped result of query for Option[C]
-  def firstOptionRef[C](implicit tag: ClassTag[C]): Option[C] = {
-    val optional = this.query.result.headOption.save
-    if (optional.isDefined) {
-      Reflect.FromTo[C](optional.get)
-    } else {
-      None
-    }
-  }
-
   // Generates query of any operation for Rep[Boolean]
   def any[Q <: Rep[_]](expr: T => Q)(implicit wt: CanBeQueryCondition[Q]): Rep[Boolean] = {
     this.query.filter(expr).length > 0
@@ -45,18 +35,5 @@ protected abstract class DbQueryExtender[+T, E](db: DatabaseDef, query: Query[T,
   // Generates query of count operation
   def count: Rep[Int] = {
     this.query.length
-  }
-
-  // Generates mapped result of query for custom type C
-  def toRef[C](implicit tag: ClassTag[C]): Seq[C] = {
-    var objSet: Seq[C] = Seq()
-
-    this.query.result.save.foreach(entity => {
-      val reflected = Reflect.FromTo[C](entity)
-      if(reflected.isDefined) {
-        objSet = objSet :+ reflected.get
-      }
-    })
-    objSet
   }
 }
