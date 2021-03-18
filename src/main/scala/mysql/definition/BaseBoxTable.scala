e69fef88603cd149aee2e96b7ae500820c587341
Mustafa Nevfel <me@mnevfel.com>
2021-03-19 02:01:39 +0300

    Reflection changes applied for generic name.

--- src/main/scala/mysql/definition/BaseBoxTable.scala
+++ src/main/scala/mysql/definition/BaseBoxTable.scala
@@ -12,7 +12,7 @@ import scala.reflect.ClassTag
     2- Customized table names. Ex: "Product", "Category", "ProductCategory" etc.
   */
 abstract class BaseBoxTable[E <: BaseBox: ClassTag](tag: Tag, table: String = "")
-  extends Table[E](tag, if(table.isEmpty) classManifest[E].erasure.getName.split('.').last.replace("ET", "").replace("Table", "") else table) {
+  extends Table[E](tag, if(table.isEmpty) implicitly[ClassTag[E]].runtimeClass.getSimpleName.split('.').last.replace("ET", "").replace("Table", "") else table) {
   def ID: Rep[Long] = column[Long]("ID", O.PrimaryKey, O.AutoInc)
 }
 
